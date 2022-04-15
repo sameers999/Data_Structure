ec1e72fdc6f22c54ef27c401aacfac0d68f53ce5
sameers999 <syedmahammadsameer999>
2022-04-15 16:22:39 +0530

    Added LinkedList

---
+++ MainClass.java
@@ -0,0 +1,25 @@
+package com.bridgelabz.linklist;
+
+public class MainClass {
+	
+	public static void main(String[] args) {
+		LinkedList list = new LinkedList();
+		list.insertNode(10);
+		list.insertNode(30);
+		list.insertNode(45);
+		list.insertFirst(5);
+		list.insertPosition(1, 12);
+		
+		list.print();
+		System.out.println(list);
+		list.popFirst();
+		
+		list.print();
+		System.out.println(list);
+		list.popOut(2);
+		
+		list.print();
+		
+		System.out.println(list);
+		}
+}
